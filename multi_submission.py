e5c06e0adbc58823c3c6363c5b3f8d72fc6f6e9a
mcht67 <malte.crt@googlemail.com>
2026-02-09 02:02:51 +0100

    updates multi_submission.py

--- multi_submission.py
+++ multi_submission.py
@@ -67,12 +67,37 @@ if __name__ == "__main__":
 
         # Define specific parameters
         "log.experiment_name": experiment_name,
-        "train.epochs": 5
+        "train.epochs": 10,
+        "train.train_size_batches": 20,
+        "train.val_size_batches": 5
     }
 
     # Define all lists of parameters or config files [Hyperparameters]
     hyperparams = {
-                    "train.input_feature_name": ['perch_8_pooled_embeddings']
+                    "train.input_feature_name": [
+                                                "perch_v2_cpu_audio_embeddings",
+                                                #"perch_v2_cpu_no_noise_audio_embeddings",
+                                                "birdnet_V2.3_audio_embeddings",
+                                                #"birdnet_V2.3_no_noise_audio_embeddings",
+                                                "vggish_audio_embeddings",
+                                                #"vggish_no_noise_audio_embeddings",
+                                                "perch_8_audio_embeddings",
+                                                #"perch_8_no_noise_audio_embeddings",
+                                                "yamnet_audio_embeddings",
+                                                #"yamnet_no_noise_audio_embeddings",
+                                                "beans_baseline_audio_embeddings",
+                                                #"beans_baseline_no_noise_audio_embeddings",
+                                                "EfficientNet-B1-BirdSet-XCL_audio_pooled_embeddings",
+                                                #"EfficientNet-B1-BirdSet-XCL_no_noise_audio_pooled_embeddings",
+                                                "Bird-MAE-Huge_audio_pooled_embeddings",
+                                                #"Bird-MAE-Huge_no_noise_audio_pooled_embeddings",
+                                                "AudioProtoPNet-20-BirdSet-XCL_audio_pooled_embeddings",
+                                                #"AudioProtoPNet-20-BirdSet-XCL_no_noise_audio_pooled_embeddings",
+                                                "AST-Birdset-XCL_audio_pooled_embeddings",
+                                                #"AST-Birdset-XCL_no_noise_audio_pooled_embeddings",
+                                                "Wav2Vec2-Base-BirdSet-XCL_audio_pooled_embeddings"]#,
+                                                #"Wav2Vec2-Base-BirdSet-XCL_no_noise_audio_pooled_embeddings"]
+                                            
                 }
 
     # Iterate over all combinations of parameters
