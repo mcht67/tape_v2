7781390002d26a626cec59230f917bff951fd282
mcht67 <malte.crt@googlemail.com>
2026-03-18 11:32:20 +0100

    change bind order

--- slurm_job.sh
+++ slurm_job.sh
@@ -109,7 +109,7 @@ echo "Starting execution from singularity container..."
 
 # Run the singularity container
 # singularity exec --nv --bind $DEFAULT_DIR $PROJECT_NAME-latest$container_extension ./exp_workflow.sh # GPU
-singularity exec --bind $DEFAULT_DIR $PROJECT_NAME-latest$container_extension\
-                --bind /etc/passwd:/etc/passwd \
+singularity exec --bind /etc/passwd:/etc/passwd \
                 --bind /etc/group:/etc/group \
+                --bind $DEFAULT_DIR $PROJECT_NAME-latest$container_extension \
                 ./exp_workflow.sh # CPU
