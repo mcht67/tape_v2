ffc0b6b7689e97bc4b061c5f6a392c7eedd68aca
mcht67 <malte.crt@googlemail.com>
2026-06-02 11:41:54 +0200

    reduced dataset for test

--- source/train.py
+++ source/train.py
@@ -208,7 +208,8 @@ def main():
     print(f"[INFO] HF_DATASETS_OFFLINE={os.environ.get('HF_DATASETS_OFFLINE', 'NOT SET')} (ommits updating datasets to avoid hitting rate limit on Huggingface Hub)")
    
     dataset = load_dataset_with_retry(huggingface_path, dataset_config, token=huggingface_token)
-    dataset.select(range(100)) # TODO: remove after testing
+    for split in dataset.keys():
+        dataset[split] = dataset[split].select(range(100)) # TODO: remove after testing
      # TODO: reset after testing
     # from datasets import load_dataset, DatasetDict, Dataset
     # print(f"Loading dataset {huggingface_path} with config {dataset_config} from Huggingface Hub...")
