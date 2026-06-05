51c0febcf9651e56ed27f09dd54bbe3b304641fc
mcht67 <malte.crt@googlemail.com>
2026-06-05 12:35:58 +0200

    pooled embeddings run on PER

--- multi_submission.py
+++ multi_submission.py
@@ -223,7 +223,7 @@ if __name__ == "__main__":
     ##########################
     
     # Load study configuration
-    study_config_path = 'study_conf/spatial_embeddings.yaml'
+    study_config_path = 'study_conf/pooled_embeddings.yaml'
 
     with open(study_config_path) as f:
         study_config = yaml.safe_load(f)
