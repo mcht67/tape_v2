29f6fbf7f6a0b49438e9fc6ba3ccc2d44ea01907
mcht67 <malte.crt@googlemail.com>
2026-06-30 21:26:03 +0200

    fixes passing self twice

--- source/utils/logs.py
+++ source/utils/logs.py
@@ -776,7 +776,7 @@ class CustomSummaryWriterCallback(tf.keras.callbacks.Callback):
         ):
             for spec in self.confusion_matrix_specs:
                 self._log_confusion_matrix(epoch, spec)
-                self._log_f1_breakdown(self, epoch, spec)   
+                self._log_f1_breakdown(epoch, spec)   
 
         # 3. DVCLive — log all metrics every epoch
         if self.live:
