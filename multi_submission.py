9cbfe8c82436851bad93073819b8e8ced75c6345
mcht67 <malte.crt@googlemail.com>
2026-07-21 19:26:18 +0200

    run dataset preparation

--- multi_submission.py
+++ multi_submission.py
@@ -245,7 +245,7 @@ if __name__ == "__main__":
     create_study_remote(study_name, base_remote="base-remote")
     
     # Dataset preparation options
-    run_dataset_preparation = False #if embeddings else False
+    run_dataset_preparation = True #if embeddings else False
     recompute_embeddings = False #if embeddings else False
     force_redownload = False
 
