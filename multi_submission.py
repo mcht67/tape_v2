418ba5cd78cb0ab011927d351ec3010ad6e56622
mcht67 <malte.crt@googlemail.com>
2026-07-29 21:18:50 +0200

    recompute and reurn perch_v2 on UHH

--- multi_submission.py
+++ multi_submission.py
@@ -251,8 +251,8 @@ if __name__ == "__main__":
     create_study_remote(study_name, base_remote="base-remote")
     
     # Dataset preparation options
-    run_dataset_preparation = False
-    recompute_embeddings = False
+    run_dataset_preparation = True
+    recompute_embeddings = True
     force_redownload = False
     run_on_gpu = False
     force_exp_rerun = True
