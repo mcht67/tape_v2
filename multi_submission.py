6c4725749574934c80a30e6c1fbfa87b2d12fa7c
mcht67 <malte.crt@googlemail.com>
2026-07-30 14:15:46 +0200

    test validation evaluation with perch_8 on UHH

--- multi_submission.py
+++ multi_submission.py
@@ -251,11 +251,11 @@ if __name__ == "__main__":
     create_study_remote(study_name, base_remote="base-remote")
     
     # Dataset preparation options
-    run_dataset_preparation = True
-    recompute_embeddings = True
+    run_dataset_preparation = False
+    recompute_embeddings = False
     force_redownload = False
     run_on_gpu = False
-    force_exp_rerun = True
+    force_exp_rerun = False
 
     ##########################
     # Submit jobs
