2373f8ddd4ef63dc832162eacda13f0cc03c80ec
mcht67 <malte.crt@googlemail.com>
2026-08-02 13:32:31 +0200

    test spatial exp perch2 UHH

--- multi_submission.py
+++ multi_submission.py
@@ -238,7 +238,7 @@ if __name__ == "__main__":
     ##########################
     
     # Load study configuration
-    study_config_path = 'study_conf/pooled_embeddings.yaml'
+    study_config_path = 'study_conf/spatial_embeddings.yaml'
 
     with open(study_config_path) as f:
         study_config = yaml.safe_load(f)
@@ -251,11 +251,11 @@ if __name__ == "__main__":
     create_study_remote(study_name, base_remote="base-remote")
     
     # Dataset preparation options
-    run_dataset_preparation = True
-    recompute_embeddings = True
+    run_dataset_preparation = False
+    recompute_embeddings = False
     force_redownload = False
     run_on_gpu = False
-    force_exp_rerun = True
+    force_exp_rerun = False
 
     ##########################
     # Submit jobs
