e20df2a633d45ca46a1457b7a91bf45b2c9c98d3
mcht67 <malte.crt@googlemail.com>
2026-08-03 09:47:40 +0200

    rerun perch2 HSN

--- source/perch_soundscape_evaluation.py
+++ source/perch_soundscape_evaluation.py
@@ -95,104 +95,104 @@ print(f"Loading weights from {checkpoint_path}")
 model.load_weights(checkpoint_path)
 
 #########################################
-    # Validation on soundscape data
-    #########################################
+# Validation on soundscape data
+#########################################
 
-    # soundscape test_5s has start/end_time and low/high_freq 
+# soundscape test_5s has start/end_time and low/high_freq 
 
-    # Load soundscape data
-    soundscape_dataset = load_dataset_with_retry(huggingface_path, soundscape_dataset_config, token=huggingface_token)
+# Load soundscape data
+soundscape_dataset = load_dataset_with_retry(huggingface_path, soundscape_dataset_config, token=huggingface_token)
 
-    # TODO: Add labels for multi task setup
-    print(soundscape_dataset['test_5s'])
-    # Polyphony degree can not be computed for soundscapes directly, but we can compute a minimum and maximum polyphony degree
+# TODO: Add labels for multi task setup
+print(soundscape_dataset['test_5s'])
+# Polyphony degree can not be computed for soundscapes directly, but we can compute a minimum and maximum polyphony degree
 
-    # Add polyphony range labels to soundscape dataset
-    # soundscape_dataset = soundscape_dataset.map(add_polyphony_range)
-   
-    # Minimum polyphony degree: get maximum number of overlapping events at any time
-    # Minimum polyphony degree: get number of species active in soundscape
-    # Maximum polyphony degree: get total number of events that occur in the soundscape
+# Add polyphony range labels to soundscape dataset
+# soundscape_dataset = soundscape_dataset.map(add_polyphony_range)
 
-    # Check if embeddings have been precomputed
-    embeddings_precomputed = input_feature_name in soundscape_dataset['test_5s'].features
+# Minimum polyphony degree: get maximum number of overlapping events at any time
+# Minimum polyphony degree: get number of species active in soundscape
+# Maximum polyphony degree: get total number of events that occur in the soundscape
 
-    # Prepare embedding model if necessary
-    if not embeddings_precomputed:
+# Check if embeddings have been precomputed
+embeddings_precomputed = input_feature_name in soundscape_dataset['test_5s'].features
 
-        embedding_type = cfg.embeddings.type
+# Prepare embedding model if necessary
+if not embeddings_precomputed:
+
+    embedding_type = cfg.embeddings.type
+    
+    if embedding_type=='birdset':
+
+        # Request GPU
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"Using device: {device}")
+
+        # Define embedding model
+        embedding_model_cfg = cfg.embeddings.model_cfg
+        embedding_model = instantiate(embedding_model_cfg)
+
+        if hasattr(embedding_model, 'to'):
+            embedding_model = embedding_model.to(device) 
+    elif embedding_type=='perch_hoplite' or embedding_type=='perch_v2':
+
+        model_key = cfg.embeddings.name
+
+        # Auto detect gpu
+        gpus = tf.config.list_physical_devices('GPU')
+        device = '/GPU:0' if gpus else '/CPU:0'
+        print(f"Using device: {device}")
+
+        # Prevent TF from grabbing all GPU memory at once
+        if gpus:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
         
-        if embedding_type=='birdset':
-
-            # Request GPU
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            print(f"Using device: {device}")
-
-            # Define embedding model
-            embedding_model_cfg = cfg.embeddings.model_cfg
-            embedding_model = instantiate(embedding_model_cfg)
-
-            if hasattr(embedding_model, 'to'):
-                embedding_model = embedding_model.to(device) 
-        elif embedding_type=='perch_hoplite' or embedding_type=='perch_v2':
-
-            model_key = cfg.embeddings.name
-
-            # Auto detect gpu
-            gpus = tf.config.list_physical_devices('GPU')
-            device = '/GPU:0' if gpus else '/CPU:0'
-            print(f"Using device: {device}")
-
-            # Prevent TF from grabbing all GPU memory at once
-            if gpus:
-                for gpu in gpus:
-                    tf.config.experimental.set_memory_growth(gpu, True)
-            
-            # Load model
-            if embedding_type == 'perch_hoplite':
-                model, sampling_rate = load_perch1_model(model_key)
-            elif embedding_type == 'perch_v2':
-                model, sampling_rate = load_perch2_model(model_key)
-            elif embedding_type == 'birdset':
-                load_birdset_model = load_birdset_model(model_key)
-            else:
-                print(f"Model family unknown. Can not load model {model_key}. Skipping.")
-                
-
-
-
-    polyphony_range_logits = []
-    distances_to_min_polyphony = []
-
-    # Get predictions and metrics on soundscape data   
-    for idx, example in enumerate(islice(soundscape_dataset['test_5s'])):
-
-        if embeddings_precomputed:
-            embedding = example[input_feature_name]
+        # Load model
+        if embedding_type == 'perch_hoplite':
+            model, sampling_rate = load_perch1_model(model_key)
+        elif embedding_type == 'perch_v2':
+            model, sampling_rate = load_perch2_model(model_key)
+        elif embedding_type == 'birdset':
+            load_birdset_model = load_birdset_model(model_key)
         else:
-            # Compute embedding for example
-            embedding = compute_embedding(example[input_feature_name], model)
-        
-        # Make prediction
-        single_input = np.expand_dims(embedding, axis=0)
-        single_input = tf.constant(single_input, dtype=tf.float32)
-        predictions = model.predict(single_input)
-        pred_polyphony = predictions['polyphony_degree'][0][0]
+            print(f"Model family unknown. Can not load model {model_key}. Skipping.")
+            
 
-        # Get polyphony logit
-        gt_min_polyphony = example['min_polyphony']
-        gt_max_polyphony = example['max_polyphony']
-        polyphony_range_logit = int(gt_min_polyphony <= pred_polyphony <= gt_max_polyphony)
-        polyphony_range_logits.append(polyphony_range_logit)
 
-        # Get distance to min polyphony
-        distance_to_min_polyphony = abs(pred_polyphony - gt_min_polyphony)
-        distances_to_min_polyphony.append(distance_to_min_polyphony)
 
-    # Get metrics on soundscape data
-    polyphony_range_accuracy = np.mean(polyphony_range_logit)
-    mean_distance_to_min_polyphony = np.mean(distances_to_min_polyphony)
+polyphony_range_logits = []
+distances_to_min_polyphony = []
 
-    # Write metrics to TensorBoard
-    writer.add_scalar('soundscape/polyphony_range_accuracy', polyphony_range_accuracy, global_step=0)
-    writer.add_scalar('soundscape/mean_distance_to_min_polyphony', mean_distance_to_min_polyphony, global_step=0)
+# Get predictions and metrics on soundscape data   
+for idx, example in enumerate(islice(soundscape_dataset['test_5s'])):
+
+    if embeddings_precomputed:
+        embedding = example[input_feature_name]
+    else:
+        # Compute embedding for example
+        embedding = compute_embedding(example[input_feature_name], model)
+    
+    # Make prediction
+    single_input = np.expand_dims(embedding, axis=0)
+    single_input = tf.constant(single_input, dtype=tf.float32)
+    predictions = model.predict(single_input)
+    pred_polyphony = predictions['polyphony_degree'][0][0]
+
+    # Get polyphony logit
+    gt_min_polyphony = example['min_polyphony']
+    gt_max_polyphony = example['max_polyphony']
+    polyphony_range_logit = int(gt_min_polyphony <= pred_polyphony <= gt_max_polyphony)
+    polyphony_range_logits.append(polyphony_range_logit)
+
+    # Get distance to min polyphony
+    distance_to_min_polyphony = abs(pred_polyphony - gt_min_polyphony)
+    distances_to_min_polyphony.append(distance_to_min_polyphony)
+
+# Get metrics on soundscape data
+polyphony_range_accuracy = np.mean(polyphony_range_logit)
+mean_distance_to_min_polyphony = np.mean(distances_to_min_polyphony)
+
+# Write metrics to TensorBoard
+writer.add_scalar('soundscape/polyphony_range_accuracy', polyphony_range_accuracy, global_step=0)
+writer.add_scalar('soundscape/mean_distance_to_min_polyphony', mean_distance_to_min_polyphony, global_step=0)
