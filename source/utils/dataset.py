7ebb3e54ac455891c80550faea5f21ac43987de4
mcht67 <malte.crt@googlemail.com>
2026-08-05 22:43:13 +0200

    rerun spatial NatureLM XCM multi exps

--- source/utils/dataset.py
+++ source/utils/dataset.py
@@ -341,7 +341,7 @@ def get_birdset_id2label(subset, dataset=None):
     #     return {birdset_id: None for birdset_id in sorted(unique_birdset_ids)}
 
 
-def add_labels(dataset, labels, birdset_id2label=None, time_dim=None, freq_dim=None, batch_size=100):
+def add_labels(dataset, labels, birdset_id2label=None, time_dim=None, freq_dim=None, batch_size=50):
     added_labels = []
 
     # Segment-wise polyphony based label
