160e6b9fbf3b173241f98551512f0daa82fd774e
mcht67 <malte.crt@googlemail.com>
2026-08-06 09:39:54 +0200

    remove disable_chacing()

--- source/evaluate_on_soundscape_data.py
+++ source/evaluate_on_soundscape_data.py
@@ -21,7 +21,7 @@ from utils.torch_evaluation import load_torch_model_for_eval, collect_prediction
 
 # Disable caching to avoid huggingface caching issues when running multiple experiments in parallel
 from datasets import disable_caching
-disable_caching()
+# disable_caching()
 
 def main():
 
