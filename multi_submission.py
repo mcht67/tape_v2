a85f7771e715fea15ae0bce05b4b43ef239feba6
mcht67 <malte.crt@googlemail.com>
2026-08-24 20:31:01 +0200

    run EfficientNEt fine-tun

--- multi_submission.py
+++ multi_submission.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     ##########################
     
     # Load study configuration
-    study_config_path = 'study_conf/spatial_embeddings.yaml'
+    study_config_path = 'study_conf/fine_tune_torch.yaml'
 
     with open(study_config_path) as f:
         study_config = yaml.safe_load(f)
@@ -255,7 +255,7 @@ if __name__ == "__main__":
     run_dataset_preparation = False
     recompute_embeddings = False
     force_redownload = False
-    run_on_gpu = False
+    run_on_gpu = True
     force_exp_rerun = False
 
     ##########################
