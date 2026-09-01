039b1f1bfd0c566cca7809199965d92b5277e6a1
mcht67 <malte.crt@googlemail.com>
2026-09-01 10:04:16 +0200

    run BirdMAE XCM spatial reg

--- multi_submission.py
+++ multi_submission.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     ##########################
     
     # Load study configuration
-    study_config_path = 'study_conf/fine_tune_torch.yaml'
+    study_config_path = 'study_conf/spatial_embeddings.yaml'
 
     with open(study_config_path) as f:
         study_config = yaml.safe_load(f)
