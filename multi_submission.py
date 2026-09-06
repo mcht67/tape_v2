c95157272e2e279a866e45e0a563a1b7e38b1dc2
mcht67 <malte.crt@googlemail.com>
2026-09-06 13:42:23 +0200

    run perch_v2 fine-tune XCM-generalization

--- multi_submission.py
+++ multi_submission.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     ##########################
     
     # Load study configuration
-    study_config_path = 'study_conf/XCM_generalization_fine_tune.yaml'
+    study_config_path = 'study_conf/XCM_generalization_fine_tune_perch.yaml'
 
     with open(study_config_path) as f:
         study_config = yaml.safe_load(f)
